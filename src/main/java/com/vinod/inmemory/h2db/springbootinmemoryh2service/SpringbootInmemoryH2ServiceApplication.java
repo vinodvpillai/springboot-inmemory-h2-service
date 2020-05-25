04038d03219904ee1b708dae460dbbf12b967f1e
Vinod Pillai <vinodthebest@gmail.com>
2020-05-25 13:46:30 +0530

    Added basic main class.
    - SpringbootInmemoryH2ServiceApplication Main Class

---
+++ src/main/java/com/vinod/inmemory/h2db/springbootinmemoryh2service/SpringbootInmemoryH2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vinod.inmemory.h2db.springbootinmemoryh2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInmemoryH2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInmemoryH2ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinod/inmemory/h2db/springbootinmemoryh2service/SpringbootInmemoryH2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinod.inmemory.h2db.springbootinmemoryh2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInmemoryH2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
